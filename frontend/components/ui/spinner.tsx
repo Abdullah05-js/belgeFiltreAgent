f65282f1bb7ecb534cfacf8821ec0dbeaf4b7cb4
thodex <abdallhameed2005@gmail.com>
2026-01-16 21:28:57 +0300

    added docker

--- frontend/components/ui/spinner.tsx
+++ frontend/components/ui/spinner.tsx
@@ -1,11 +1,22 @@
+import * as React from "react"
 import { cn } from "@/lib/utils"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { Loading03Icon } from "@hugeicons/core-free-icons"
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
-  return (
-    <HugeiconsIcon icon={Loading03Icon} strokeWidth={2} role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
-  )
-}
+const Spinner = React.forwardRef<SVGSVGElement, React.ComponentPropsWithoutRef<"svg">>(
+  ({ className, strokeWidth, ...props }, ref) => {
+    return (
+      <HugeiconsIcon
+        ref={ref}
+        icon={Loading03Icon}
+        // Tipi garantiye alıyoruz
+        strokeWidth={typeof strokeWidth === 'number' ? strokeWidth : 2}
+        className={cn("size-4 animate-spin", className)}
+        {...props}
+      />
+    )
+  }
+)
+Spinner.displayName = "Spinner"
 
 export { Spinner }
